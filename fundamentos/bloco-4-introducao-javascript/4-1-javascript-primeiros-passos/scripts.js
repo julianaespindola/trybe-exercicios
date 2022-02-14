6a652a8de763467df9831bdbda28cfef091b77b4
juliana-espindola <j.lespindolaa@gmail.com>
2022-02-13 21:31:03 -0400

    Exercicios 4.1 JS primeiros passos

--- fundamentos/bloco-4-introducao-javascript/4-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-javascript/4-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,46 @@
+const myName = 'Juliana';
+const birthCity = 'Campo Grande';
+const birthYear = 1988;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 
-const a = 12;
-const b = 7;
+const base= 5;
+const height = 8;
+const area = base * height;
+console.log(area);
 
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
+const perimeter = 5 + 5 + 8 +8;
+console.log(perimeter);
+
+const notaDesafio = 80;
+
+if(notaDesafio >= 80){
+   console.log("Parabéns, você foi aprovada(o)!")
+}else if(notaDesafio < 80 && notaDesafio >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else if(notaDesafio < 60) {
+    console.log("Você foi reprovada(o)")
+};
+
+const currentHour = 22;
+let message = '';
+if(currentHour >= 22){
+    message = 'Não deveríamos comer nada, é hora de dormir';
+} else if(currentHour >= 18 && currentHour < 22){
+    message = 'Rango da noite, vamos jantar :D';
+} else if (currentHour >= 14 && currentHour < 18){
+    message ='Vamos fazer um bolo pro café da tarde?';
+} else if(currentHour >= 11 && currentHour < 14){
+    message ='Hora do almoço!!!';
+} else if( currentHour < 11 && currentHour < 4){
+    message ='Hmmm, cheiro de café recém passado';
+} 
+console.log(message)
+
+const weekDay = 'sabado';
+if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' ||weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay ==='sexta-feira'){
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+}else {
+    console.log('FINALMENTE, descanso merecido UwU');
+}
